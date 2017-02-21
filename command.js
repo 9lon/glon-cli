be8a4d57e1414a6cf96d8a4442023295e876f1ad
Onuma Cheawchan <Onuma Cheawchan>
2017-02-21 11:13:19 +0700

    update 1.3.0

--- command.js
+++ command.js
@@ -26,7 +26,7 @@ function app_server(){
 }
 
 program
-  .version('1.2.9')
+  .version('1.3.0')
   .option('-i, init', 'init project',app_init)
   .option('-s, serve', 'start nylon server',app_server)
   .parse(process.argv);
